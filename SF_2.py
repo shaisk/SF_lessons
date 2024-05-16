94d68a0c00ea7367043b501e08b2fcba8b39dd39
shaisk <shaisikama@gmail.com>
2024-05-16 21:14:34 +0300

    add SF_17.7.3

---
+++ SF_2.py
@@ -0,0 +1,22 @@
+def calculate_ticket_price(age):
+    if age < 18:
+        return 0
+    elif 18 <= age < 25:
+        return 990
+    else:
+        return 1390
+
+def main():
+    num_tickets = int(input("Введите количество билетов, которые вы хотите приобрести: "))
+    total_cost = 0
+    for _ in range(num_tickets):
+        age = int(input("Введите возраст посетителя: "))
+        ticket_price = calculate_ticket_price(age)
+        total_cost += ticket_price
+
+    if num_tickets > 3:
+        total_cost *= 0.9  # Применяем 10% скидку, если билетов больше трех
+
+    print("Сумма к оплате:", total_cost, "руб.")
+
+main()
